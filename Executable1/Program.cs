55ad010e3b6b334b61584c3fd7369874195a3495
Michael Dietrich <dietrich@zeiss-izm.de>
2020-06-02 13:53:23 +0200

    Add sample code for reproducing the issue

---
+++ Executable1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Executable1
+{
+	static class Program
+	{
+		public static void Main()
+		{
+			//var smth = Microsoft.IdentityModel.JsonWebTokens.JwtConstants.DirectKeyUseAlg;
+		}
+	}
+}
---
+++ Executable2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Executable2
+{
+	static class Program
+	{
+		public static void Main()
+		{
+			var smth = new Microsoft.IdentityModel.Tokens.JsonWebKey();
+		}
+	}
+}
